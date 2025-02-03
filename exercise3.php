d87365a95babde874a3565166236987c907f33a8
AlexxLen <lenchenkov.a.d@gmail.com>
2025-02-03 22:34:35 +0500

    task1 | Refactored third task

--- exercise3.php
+++ exercise3.php
@@ -2,6 +2,9 @@
 
 declare(strict_types=1);
 
+$startTime = microtime(true);
+$startMemory = memory_get_usage();
+
 $prefix = 'con';
 $arr = [
 	'clinic', 'contract', 'abstract', 'public', 'consumer', 'test', 'contributor', 'concat', 'cat'
@@ -12,15 +15,19 @@ $result = filterStringsByPrefix($prefix, $arr);
 
 var_dump($result);
 
+$endTime = microtime(true);
+$endMemory = memory_get_usage();
+
+$executionTime = $endTime - $startTime;
+$memoryUsage = $endMemory - $startMemory;
+
+echo 'Execution time: ' . $executionTime . ' seconds' . PHP_EOL;
+echo 'Memory usage: ' . $memoryUsage . 'bites' . PHP_EOL;
+
 function filterStringsByPrefix(string $prefix, array $arr): array
 {
-	$result = [];
-
-	foreach ($arr as $item) {
-		if (preg_match("/{$prefix}/", $item)) {
-			$result[] = $item;
-		}
-	}
-	return $result;
+	return array_filter($arr, function ($item) use ($prefix) {
+		return strpos($item, $prefix) === 0;
+	});
 }
 
